3d5dc13785f01bcf786f144fda70bbef94fa0f5c
Volodymyr Rodin <vladrodin.en@gmail.com>
2024-08-31 20:09:26 +0200

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I'm currently a beginner software engineer and data scientist, working at Vespia.
+      Lately, I've been diving into programming languages like JavaScript, Python, and C. Regardless of the language, I just enjoy coding.
+      Here's my <a href="https://github.com/uoloki" target="_blank">Github</a>!`,
+    "cs-degree": `This is where my future degree will hang. I'm working hard to earn it, and I believe it'll provide a solid foundation for my career.`,
+    "sofa-table": `That's my sofa. I like to unwind here and watch Dota 2 matches or dive into some programming tutorials.`,
+    tv: `That's my TV. Lately, I've been watching a lot of tech YouTubers and keeping up with cybersport tournaments.
+     Some of my favorite tech channels include <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, 
+     <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>, and <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a>!`,
+    bed: `This is where I sleep. I often get my best ideas when I'm lying in bed. Sometimes, I have to write them down right away so I can get some sleep.`,
+    resume: `This is my desk, and on it is my resume.
+    You can reach me at uoloki@protonmail.com if you have any exciting opportunities in software engineering or data science!`,
+    projects: `Info about this portfolio: It's a work in progress as I continue learning and improving. 
+    I'm using a mix of HTML/CSS and JavaScript, but I'm constantly experimenting with new tools and libraries.`,
+    library: `These shelves are filled with programming books. There's even one in Russian (I speak Russian too). 
+    I've read a few of them, but who doesn't buy more technical books than they can finish?`,
+    exit: `If you want to exit Uoloki's portfolio, just close the tab.`,
+  };
+  
